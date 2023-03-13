febedb749cb5310bf43791b9f6340df4aefc2578
brut4luk3 <lucasreinert96@gmail.com>
2023-03-13 17:55:43 -0300

    V1A1 - Em ajustes

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 from csv import *
+from pathlib import Path
 from tkinter import *
-from tkinter import messagebox
 import tkinter as tk
 
 main = tk.Tk()
 main.title('Bye Bye Excel')
-main.geometry("1000x550")
+main.geometry("900x550")
 lista_principal = []
 
 #FUNÇÕES
-def Add():
+def CreateAndAdd():
+    lista = []
+
+    if bool(lista):
+        pass
+    else:
+        frame_visualizacao.delete(1.0, END)
+
+    #CRIA A SINTAXE JSON
     chave_json = "retorno"
     dois_pontos = ":"
     chave_1 = "{"
@@ -24,59 +32,108 @@ def Add():
     saida_formatada = f'{chave_1}{aspas_1}{chave_json}{aspas_2}{dois_pontos} {aspas_3}{saida}{aspas_4}{chave_2}'
 
     lista = [entrada, saida_formatada]
-    lista_principal.append(lista)
-    messagebox.showinfo('Informação', 'Dados adicionados com sucesso!')
 
-def Save():
+    lista_principal.append(lista)
+    #CRIA A SINTAXE JSON - FIM
+
+    #SALVA O DOCUMENTO
     with open('data_entry.csv', 'w', newline='') as file:
         Writer = writer(file)
         Writer.writerows(lista_principal)
-        messagebox.showinfo('Informação', 'Planilha salva com sucesso!')
+    #SALVA O DOCUMENTO - FIM
 
-def Clear():
+    #VERIFICA SE EXISTE ARQUIVO
+    caminho = Path('./data_entry.csv')
+    verifica_arquivo = caminho.is_file()
+    #VERIFICA SE EXISTE ARQUIVO - FIM
+
+    #EXIBE O ARQUIVO CRIADO (SE EXISTIR)
+    if verifica_arquivo == True:
+        visualizar_doc = open("data_entry.csv", 'r')
+        acao_visualizar_doc = visualizar_doc.read()
+        frame_visualizacao.insert(END, acao_visualizar_doc)
+
+        visualizar_doc.close()
+    elif verifica_arquivo != True:
+        pass
+    #EXIBE O ARQUIVO CRIADO (SE EXISTIR) - FIM
+
+    #LIMPA OS CAMPOS ENTRY
     caixa_entrada.delete(0, END)
     caixa_saida.delete(0, END)
+    #LIMPA OS CAMPOS ENTRY - FIM
+
+def ClearLast():
+    #VERIFICA SE EXISTE ARQUIVO
+    caminho_clearLast = Path('./data_entry.csv')
+    verifica_arquivo_clearLast = caminho_clearLast.is_file()
+    #VERIFICA SE EXISTE ARQUIVO - FIM
+
+    #APAGA A ÚLTIMA LINHA
+    if verifica_arquivo_clearLast == True:
+        linhas = open("data_entry.csv", 'r+')
+        acao_linhas = linhas.readlines()
+        linhas.seek(0)
+        linhas.truncate()
+        linhas.writelines(acao_linhas[:-1])
+
+        linhas.close()
+
+        frame_visualizacao.delete(1.0, END)
+
+        ler_linhas = open("data_entry.csv", 'r')
+        acao_ler_linhas = ler_linhas.read()
+        frame_visualizacao.insert(END, acao_ler_linhas)
+
+        ler_linhas.close()
+
+    elif verifica_arquivo_clearLast != True:
+        pass
+    #APAGA A ÚLTIMA LINHA - FIM
 #FUNÇÕES - FIM
 
 #ÁREA DA LOGO
 logo = PhotoImage(file='./byebye_excel_sem_fundo1.png')
 image_label = tk.Label(image=logo)
-image_label.pack()
+image_label.place(x=50, y=20)
 #ÁREA DA LOGO - FIM
 
+#ÁREA DA VISUALIZAÇÃO DE DADOS NA PLANILHA
+texto_visualizacao_label = tk.Label(text='Visualize as linhas adicionadas na planilha:', font=(35), pady=10)
+texto_visualizacao_label.place(x=500, y=20)
+
+frame_visualizacao = Text(main, width=45, height=28)
+frame_visualizacao.place(x=500, y=55)
+#ÁREA DA VISUALIZAÇÃO DE DADOS NA PLANILHA - FIM
 
 #ÁREA DO TEXTO DE ENTRADA
 texto_entrada = 'Digite o dado de entrada da planilha:'
 texto_entrada_label = tk.Label(text=texto_entrada, font=(35), pady=10)
-texto_entrada_label.pack()
+texto_entrada_label.place(x=50, y=250)
 #ÁREA DO TEXTO DE ENTRADA - FIM
 
 #INPUT DE ENTRADA
 caixa_entrada = tk.Entry(width=50, borderwidth=5)
-caixa_entrada.pack()
+caixa_entrada.place(x=50, y=290)
 #INPUT DE ENTRADA - FIM
 
 #ÁREA DO TEXTO DE SAÍDA
-texto_saida = 'Digite o retorno da planilha dentro das aspas:'
+texto_saida = 'Digite o dado de saída da planilha:'
 texto_saida_label = tk.Label(text=texto_saida, font=(35), pady=10)
-texto_saida_label.pack()
+texto_saida_label.place(x=50, y=330)
 #ÁREA DO TEXTO DE SAÍDA - FIM
 
 #INPUT DE SAÍDA
 caixa_saida = tk.Entry(width=50, borderwidth=5)
-caixa_saida.pack()
+caixa_saida.place(x=50, y=370)
 #INPUT DE SAÍDA - FIM
 
 #BOTÕES
-save = Button(main,text='Salvar e finalizar',padx=20,pady=10,command=Save)
-add = Button(main,text='Adicionar nova linha',padx=20,pady=10,command=Add)
-clear = Button(main,text='Limpar',padx=18,pady=10,command=Clear)
-Exit = Button(main,text='Sair',padx=20,pady=10,command=main.quit)
+CreateAndAdd = Button(main,text='Adicionar nova linha',padx=20,pady=10,command=CreateAndAdd)
+clearLast = Button(main,text='Apagar última linha',padx=18,pady=10,command=ClearLast)
 
-save.pack()
-add.pack()
-clear.pack()
-Exit.pack()
+CreateAndAdd.place(x=100, y=410, width=200)
+clearLast.place(x=100, y=460, width=200)
 #BOTÕES - FIM
 
 main.mainloop()
